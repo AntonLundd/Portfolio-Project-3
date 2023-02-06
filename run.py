11a280b1e9422f5bd1f10b009b8a055ae2323689
AntonLundd <anton.lundberg1337@gmail.com>
2023-02-06 15:23:21 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+Imports random int for placement of ships.
+"""
+import random
+
+
+class Game:
+    """
+    Contains main part of game logic.
+    """
+    def __init__(self):
+        self.board = []
+        self.ships = []
+        self.guesses = []
